1178f2f82ea77f8a900f2294f344af803f149003
anglesideangel <102494188+anglesideangel@users.noreply.github.com>
2024-05-26 14:45:17 -0700

    finished leave one out

--- validator.py
+++ validator.py
@@ -1,9 +1,26 @@
 import numpy as np
-
+from classifier import classifier
 class validator:
     
-    def leave_one_out(data):
-        predicted_values = []
-        data_classes = [entry[0] for entry in data]
-        for index in range(len(data)):
-            test_data = data[index]
+    def __init__(self,classifier,dataset):
+        self.classifier = classifier
+        self.dataset = dataset
+
+    def leave_one_out(self,feature_subset):
+        prediction = 0
+        total_instances = len(self.data)
+
+        for i in range(total_instances):
+            training_data = np.delete(self.data,i,axis=0)
+            test_instance = self.data[i]
+            train_classifier = NN(features=feature_subset,filename=None)
+            train_classifier.data = training_data
+
+            predicted_class = train_classifier.test(test_instance[1:])
+            actual_class = test_instance[0]
+
+            if predicted_class == actual_class:
+                prediction +=1
+        
+        accuracy = prediction/total_instances
+        return accuracy
